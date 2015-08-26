f0b94ebe1e7e6e692968e2032ab6fc4591d42d7e
Bernard Mordan <bernardmordan@gmail.com>
2015-08-26 11:50:07 +0100

    first version of pdflippers

---
+++ readme.md
@@ -0,0 +1,10 @@
+# TABLEFLIP headed paper
+
+This module will turn a .md file into a .pdf and wrap the output in TABLEFLIP branded header and footers. Clone this repo and from within the project folder run the following commands
+
+## Usage
+
+```
+node render [relative/path/to/file.md] [optional desination]
+```
+The default desination for the generated file is your Desktop
---
+++ render.js
@@ -0,0 +1,34 @@
+var markdawn = require('markdawn')
+var fs = require('fs')
+var path = require('path')
+var exec = require('child_process').exec
+
+var pathToDoc = process.argv[2]
+var destination = process.argv[3] || '~/Desktop/'
+var filename = 'TABLEFLIP-' + path.parse(pathToDoc).name + '.pdf'
+
+fs.readFile(pathToDoc, function (err, content) {
+  if (err) throw new Error(err)
+  markdawn.generate(content.toString(), {
+    index: './theme/index.html',
+    out: filename
+  })
+  openFile(filename)
+})
+
+function openFile (filename) {
+  fs.open(filename, 'r', function (err, exists) {
+    if (err) throw new Error(err)
+    if (exists) {
+      var cmd = 'mv ' + filename + ' ' + destination+filename + ' && open ' + destination+filename
+      exec(cmd, function (err) {
+        if (err) throw new Error(err)
+        process.exit()
+      })
+    }else{
+      setTimeout(function () {
+        openFile(filename)
+      }, 250)
+    }
+  })
+}
